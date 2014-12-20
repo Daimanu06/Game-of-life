bcb874a37dd1e467268ed06e43f45086b591b3d5
Daimanu06 <john.doe@mail.com>
2014-12-20 21:46:09 +0100

    Updated terminal renderer.

--- terminalrenderer.cpp
+++ terminalrenderer.cpp
@@ -2,21 +2,33 @@
 #include "grid.h"
 #include "cell.h"
 #include <iostream>
+#ifdef __gnu_linux__
+#include <sys/ioctl.h>
+#endif
 
 namespace gameoflife {
 
 	void TerminalRenderer::draw(const Grid &grid) {
-		for(Size::size_t row = 0; row < grid.width(); row++) {
-			for(Size::size_t col = 0; col < grid.height(); col++) {
-				std::cout << (grid(row, col).isAlive() ? 'X' : ' ') << ' ';
+		for(Size::size_t row = 0; row < grid.height(); row++) {
+			for(Size::size_t col = 0; col < grid.width(); col++) {
+				std::cout << (grid(row, col).isAlive() ? 'X' : ' ');
 			}
 			std::cout << '\n';
 		}
+		std::cout << std::string(availablespace().width, '=') << '\n';
 	}
 
-	char TerminalRenderer::waitinput() {return 0;}
+	char TerminalRenderer::waitinput() {
+		return std::cin.get();
+	}
 
 	Size TerminalRenderer::availablespace() {
-		return Size{0, 0}; //TODO: get space of terminal
+#ifdef __gnu_linux__
+		winsize w;
+		ioctl(0, TIOCGWINSZ, &w);
+		return Size{w.ws_col, w.ws_row};
+#else
+		return Size{0, 0}; //TODO: get space of terminal for other systems
+#endif
 	}
 }
--- terminalrenderer.h
+++ terminalrenderer.h
@@ -10,6 +10,8 @@ namespace gameoflife {
 	/**
 	 * View using a simple terminal.
 	 * Show one grid per generation on the default output (stdout).
+	 * @warning Does not work on every platform
+	 * @warning To enter a command, you need to type the letter and press <Enter> to validate.
 	 */
 	struct TerminalRenderer : public Renderer {
 			virtual void draw(const Grid&) override;
